c928968e4c583e84d1220930ca94ba4446b0ac25
sociosmart <gfelix@smartgasgasolineras.mx>
2026-05-04 09:57:50 -0700

    Loading inicio sesion

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.services';
-import { ToastController } from '@ionic/angular';
+import { ToastController,LoadingController  } from '@ionic/angular';
 import { Router } from '@angular/router';
 
 
@@ -16,7 +16,8 @@ export class LoginPage {
   constructor(
     private auth: AuthService,
     private toast: ToastController,
-    private router: Router
+    private router: Router,
+    private loadingCtrl: LoadingController
   ) {
     
       this.router.navigate(['/alta-activo']);
@@ -24,34 +25,53 @@ export class LoginPage {
     
 
 
-  async login() {
+async login() {
 
-    if (!this.email || !this.password) {
-      this.showToast('Completa los campos');
-      return;
-    }
-
-    this.auth.login({
-      email: this.email,
-      password: this.password
-    }).subscribe({
-      next: async (res) => {
-
-console.log(res);
-
-
-        await this.auth.setToken(res.token);
-
-        this.showToast('Login correcto');
-      const token = await this.auth.getToken();
-console.log('TOKEN GUARDADO:', token);
-this.router.navigateByUrl('/alta-activo', { replaceUrl: true });
-      },
-      error: () => {
-        this.showToast('Credenciales incorrectas');
-      }
-    });
+  if (!this.email || !this.password) {
+    this.showToast('Completa los campos');
+    return;
   }
+
+  const loading = await this.loadingCtrl.create({
+    message: 'Iniciando sesión...',
+    spinner: 'crescent'
+  });
+
+  await loading.present();
+
+  this.auth.login({
+    email: this.email,
+    password: this.password
+  }).subscribe({
+    next: async (res) => {
+
+      await loading.dismiss(); // 👈 IMPORTANTE cerrar loader
+
+      console.log(res);
+
+      await this.auth.setToken(res.token);
+
+      this.showToast('Login correcto');
+
+      const token = await this.auth.getToken();
+      console.log('TOKEN GUARDADO:', token);
+
+      this.router.navigateByUrl('/alta-activo', { replaceUrl: true });
+    },
+    error: async (err) => {
+
+      await loading.dismiss(); // 👈 cerrar loader también en error
+
+      console.log(err);
+
+      if (!navigator.onLine) {
+        this.showToast('Sin conexión a internet');
+      } else {
+        this.showToast('Credenciales incorrectas o error del servidor');
+      }
+    }
+  });
+}
 
   async showToast(msg: string) {
     const t = await this.toast.create({
